6e3187f0fe64b649c459acd9cd994b5c9606be5a
yerytech <yerytecnology@gmail.com>
2026-03-19 14:06:27 -0400

    fix: correct conditional rendering logic in RootNavigator

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { LoginScreen } from '../screens/LoginScreen';
 import { RegisterScreen } from '../screens/RegisterScreen';
 import { AuthStackParamList } from './types';
+import { SplashScreen } from '../screens/SplashScreen';
 
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
@@ -13,7 +14,7 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 export const AuthNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-
+       {/* <Stack.Screen component={SplashScreen} name="Splash" /> */}
       <Stack.Screen component={LoginScreen} name="Login" />
       <Stack.Screen component={RegisterScreen} name="Register" />
     </Stack.Navigator>
